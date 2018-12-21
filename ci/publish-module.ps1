b0ce3703854fa30464be93d3bf83e1b4f45a71f6
Bin Yi <byi@sumologic.com>
2018-12-21 13:57:30 -0800

    optimize publishing script

--- ci/publish-module.ps1
+++ ci/publish-module.ps1
@@ -1,11 +1,35 @@
+$finalVersion = ""
+
+if(-not [string]::IsNullOrWhiteSpace($env:APPVEYOR_REPO_TAG_NAME)){
+  $finalVersion = $env:APPVEYOR_REPO_TAG_NAME
+} else {
+  $finalVersion = $env:APPVEYOR_BUILD_VERSION
+}
+
+Write-Host "APPVEYOR_REPO_TAG_NAME = $env:APPVEYOR_REPO_TAG_NAME"
+Write-Host "APPVEYOR_BUILD_VERSION = $env:APPVEYOR_BUILD_VERSION"
+
+if([string]::IsNullOrWhiteSpace($finalVersion)){
+  Write-Error "Unable to determine release version [$finalVersion]"
+  exit -1
+}
+
 if ($env:APPVEYOR_REPO_TAG -eq 'true') {
   try {
     $file = Get-Item -Path "$PSScriptRoot/../src/main/SumoLogic-Core/SumoLogic-Core.psd1"
-    $regex = "ModuleVersion\s+=\s+'(\d\.\d\.\d+)'"
-    $version = (Get-Content $file | Select-String -Pattern $regex).Matches.Groups[1].Value
-    Write-Host "Publishing to PowerShell Gallery with version $version ..."
+    $regex = "(?<=\s+ModuleVersion\s+=\s+')(\d\.\d\.\d+)(?=')"
+    $lines = Get-Content $file | ForEach-Object {
+      $line = $_
+      if ($line -match $regex) {
+        $line -replace $regex, $finalVersion
+      } else {
+        $line
+      }
+    } 
+    $lines | Out-File $file
+    Write-Host "Publishing to PowerShell Gallery with version $finalVersion ..."
     Publish-Module -Path "$PSScriptRoot/../src/main/SumoLogic-Core" -NuGetApiKey $env:NuGetApiKey -ErrorAction Stop
-    Write-Host "SumoLogic-Core PowerShell Module version $version published to the PowerShell Gallery." -ForegroundColor Green
+    Write-Host "SumoLogic-Core PowerShell Module version $finalVersion published to the PowerShell Gallery." -ForegroundColor Green
   } catch {
     Write-Warning "Fail to publish module to PowerShell Gallery"
     throw $_
--- src/main/SumoLogic-Core/SumoLogic-Core.psd1
+++ src/main/SumoLogic-Core/SumoLogic-Core.psd1
@@ -12,7 +12,7 @@
   RootModule        = 'SumoLogic-Core.psm1'
 
   # Version number of this module.
-  ModuleVersion     = '1.1.3'
+  ModuleVersion     = '0.0.0'
 
   # Supported PSEditions
   # CompatiblePSEditions = @()
